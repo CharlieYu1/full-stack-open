524a6950eabb27b8da277e08a8f130b10ce73b5f
Charlie Yu <imo.charlie@gmail.com>
2023-01-03 18:15:45 +0000

    added cors to backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
