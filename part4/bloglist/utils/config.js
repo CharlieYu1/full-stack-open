69218ce74f26d08b5b02c14047c84ff1a5ef7064
Charlie Yu <imo.charlie@gmail.com>
2023-01-07 16:07:37 +0000

    user routes

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -3,7 +3,8 @@ const PORT = process.env.PORT || 3003
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
+const SECRET = process.env.SECRET || 'NOT_FOR_PRODUCTION'
 
 module.exports = {
-  PORT, MONGODB_URI
+  PORT, MONGODB_URI, SECRET
 }
