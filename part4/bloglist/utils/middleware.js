1ab12159981f65018d8b9dd8adadd1ac1c7e5f20
Charlie Yu <imo.charlie@gmail.com>
2023-01-08 02:32:10 +0000

    exercise 4.22

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+const config = require('./config')
+const User = require('../models/user')
+
+const tokenExtractor = async (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+
+  try {
+    const decodedToken = await jwt.verify(request.token, config.SECRET)
+    request.decodedToken = decodedToken
+  } catch (error) {
+    request.decodedToken = null
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = request.decodedToken
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  try {
+    const user = await User.findById(decodedToken.id, { passwordHash: 0 })
+    request.user = user
+  } catch(error) {
+    request.user = null
+  }
+  next()
+}
+
+module.exports = { tokenExtractor, userExtractor }
